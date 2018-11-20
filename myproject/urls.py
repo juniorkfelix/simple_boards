db4d96ef59e198d17cd91a4ab99dc1bb0f5d8fae
juniorkfelix <felixonsare@gmail.com>
2018-11-21 00:48:19 +0300

    added a few edits and logon functions

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from boards import views
+from django.contrib.auth import views as auth_views
+from accounts import views as accounts_views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    
     url(r'^$', views.home, name='home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^admin/', admin.site.urls),
 ]
